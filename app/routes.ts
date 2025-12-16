940428eaa5b7eb98033f22c5c3b3ecb40cd17bc1
Ilya Hvostenko <ilyajavadev@gmail.com>
2025-12-16 07:42:04 +0200

    final

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,14 @@
-import {type RouteConfig, index, route} from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
+    route(
+        "/.well-known/appspecific/com.chrome.devtools.json",
+        "routes/devtools.ts"
+    ),
+
     index("routes/home.tsx"),
-    route('/auth', 'routes/auth.tsx'),
-    route('/upload', 'routes/upload.tsx'),
-    route('/resume/:id', 'routes/resume.tsx'),
-    route('/wipe', 'routes/wipe.tsx'),
+    route("/auth", "routes/auth.tsx"),
+    route("/upload", "routes/upload.tsx"),
+    route("/resume/:id", "routes/resume.tsx"),
+    route("/wipe", "routes/wipe.tsx"),
 ] satisfies RouteConfig;
